f3a4c159716b3094324f825089dbf1b85d9f7763
Daniel Han <zxhan2012@gmail.com>
2015-12-15 12:00:31 -0500

    use Gaussian NB to get the classfier

--- Naive Bayes/ClassifyNB.py
+++ Naive Bayes/ClassifyNB.py
@@ -1,9 +1,15 @@
-def classify(features_train, labels_train):   
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+
+def classify(features_train, labels_train):
     ### import the sklearn module for GaussianNB
     ### create classifier
     ### fit the classifier on the training features and labels
     ### return the fit classifier
-    
-        
+
     ### your code goes here!
-    pass
+    X = np.array(features_train)
+    Y = np.array(labels_train)
+    clf = GaussianNB()
+    clf.fit(X, Y)
+    return clf
