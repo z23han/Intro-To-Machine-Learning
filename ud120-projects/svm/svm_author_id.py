79a62690b7c7b1a2429eb334712a2625e24f066a
Daniel Han <zxhan2012@gmail.com>
2015-12-16 14:43:40 -0500

    svm algoritm

--- ud120-projects/svm/svm_author_id.py
+++ ud120-projects/svm/svm_author_id.py
@@ -20,11 +20,23 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
 
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+
+from sklearn.svm import SVC
+clf = SVC(C=10.0, kernel='rbf')
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+
+accuracy = accuracy_score(labels_test, pred)
+
+print accuracy
 #########################################################
 
 
