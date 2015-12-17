699dfc1e59d764dda340a51402781e94c0f5d24c
Daniel Han <zxhan2012@gmail.com>
2015-12-17 00:23:17 -0500

    decision tree general testing accuracy

--- ud120-projects/decision_tree/dt_author_id.py
+++ ud120-projects/decision_tree/dt_author_id.py
@@ -20,12 +20,23 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+X = features_train
+Y = labels_train
 
+from sklearn import tree
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(X, Y)
+
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+
+acc = accuracy_score(pred, labels_test)
+
+print acc
 #########################################################
 
 
