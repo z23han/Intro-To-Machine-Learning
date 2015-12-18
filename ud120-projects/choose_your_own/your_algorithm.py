71bf853851417ac5e20c7a1125dcb0feded5df6d
Daniel Han <zxhan2012@gmail.com>
2015-12-17 19:25:46 -0500

    different algorithms

--- ud120-projects/choose_your_own/your_algorithm.py
+++ ud120-projects/choose_your_own/your_algorithm.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+import sys
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -31,14 +32,56 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+'''
+1. k nearest neighbors
+2. random forest
+3. adaboost (also called boosted decision tree)
+'''
+from sklearn.metrics import accuracy_score
+import numpy as np
 
+X = np.array(features_train)
+Y = np.array(labels_train)
 
+opt = 2
+if opt == 0:
+    print 'run k-nearest-neighbors'
+    from sklearn import neighbors
+    clf = neighbors.KNeighborsClassifier(n_neighbors=1)
+    clf.fit(X, Y)
+    pred = clf.predict(features_test)
+    ### you fill this in!
+    acc = accuracy_score(pred, labels_test)
+    print 'knn:', acc   # 0.94
 
+elif opt == 1:
+    print 'run random-forest'
+    from sklearn import ensemble
+    clf = ensemble.RandomForestClassifier()
+    clf.fit(X, Y)
+    pred = clf.predict(features_test)
+    ### you fill this in!
+    acc = accuracy_score(pred, labels_test)
+    print 'random forest:', acc #0.916
 
+elif opt == 2:
+    print 'run adaboost'
+    from sklearn import ensemble
+    clf = ensemble.GradientBoostingClassifier()
+    clf.fit(X, Y)
+    pred = clf.predict(features_test)
+    ### you fill this in!
+    acc = accuracy_score(pred, labels_test)
+    print 'adaboost:', acc  #0.912
+
+else:
+    print 'no such algorithm. Error input for opt'
+    sys.exit()
 
 
 
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
+    print 'error in picturing!!'
     pass
