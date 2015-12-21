fc8195089ecf77d6df0cc3d3b3c2e578cbf695b3
Daniel Han <zxhan2012@gmail.com>
2015-12-21 13:01:29 -0500

    the correct way of getting scores, simply get 2 paras

--- ud120-projects/outliers/outlier_removal_regression.py
+++ ud120-projects/outliers/outlier_removal_regression.py
@@ -27,14 +27,17 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### the plotting code below works, and you can see what your regression looks like
 
 
+from sklearn import linear_model
+import numpy as np
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
+coef = reg.coef_
+intercept = reg.intercept_
 
+score = reg.score(ages_test, net_worths_test)
 
-
-
-
-
-
+print score
 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
