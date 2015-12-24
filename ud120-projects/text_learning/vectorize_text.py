5f48f10003bcadce75b496343f83489269dbb98a
Daniel Han <zxhan2012@gmail.com>
2015-12-23 21:36:01 -0500

    get from sara and chris, parse out vocabulary

--- ud120-projects/text_learning/vectorize_text.py
+++ ud120-projects/text_learning/vectorize_text.py
@@ -43,18 +43,28 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
         if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
+            path = os.path.join('C:\Users\Han\Desktop', path[:-1])
+            #print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            text = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            text = text.replace("sara", "")
+            text = text.replace("shackleton", "")
+            text = text.replace("chris", "")
+            text = text.replace("germani", "")
 
             ### append the text to word_data
+            word_data.append(text)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            if name == 'sara':
+                from_data.append(0)
+            elif name == 'chris':
+                from_data.append(1)
 
 
             email.close()
@@ -66,10 +76,8 @@ from_chris.close()
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
-
-
-
 ### in Part 4, do TfIdf vectorization here
+
+
 
 
