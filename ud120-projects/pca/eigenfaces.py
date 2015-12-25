39f16c6fc2c59c90f069e296fc0a2a1793146462
Daniel Han <zxhan2012@gmail.com>
2015-12-25 18:16:50 -0500

    eigenfaces for pca analysis

--- ud120-projects/pca/eigenfaces.py
+++ ud120-projects/pca/eigenfaces.py
@@ -66,7 +66,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+n_components = 100
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
