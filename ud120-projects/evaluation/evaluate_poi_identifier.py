1c248b25fd91203843dd8c431f0b334aa26d151c
Daniel Han <zxhan2012@gmail.com>
2015-12-26 14:01:57 -0500

    calculate precision, recall, and accuracy

--- ud120-projects/evaluation/evaluate_poi_identifier.py
+++ ud120-projects/evaluation/evaluate_poi_identifier.py
@@ -24,8 +24,31 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
-
-
 ### your code goes here 
+from sklearn import tree
+from sklearn import cross_validation
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(
+    features, labels, test_size=0.3 ,random_state=42
+)
 
 
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+acc = accuracy_score(pred, labels_test)
+print acc
+prec = precision_score(y_true=labels_test, y_pred=pred)
+print prec
+recal= recall_score(y_true=labels_test, y_pred=pred)
+print recal
+
+pred = list(pred)
+labels_test = list(labels_test)
+for i in range(len(pred)):
+    if pred[i] == 1 and labels_test[i] == 1:
+        print 'haha'
